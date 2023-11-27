fac831aae4fd4239bb960f8b4927d37c52c4fb61
Rishav <111266613+rissshaaav@users.noreply.github.com>
2023-11-27 15:52:32 +0530

    implemented posts api implemented posts api to load posts. also implemented next page and prev page buttons.

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -3,14 +3,29 @@ import styles from "./cardList.module.css";
 import Pagination from '../pagination/Pagination';
 import Card from '../card/Card';
 
-const CardList = () => {
+const getData = async (page) => {
+  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("CardList: failed to fetch posts");
+  }
+  return res.json();
+};
+
+const CardList = async({page}) => {
+  const data = await getData(page);
+  const POST_PER_PAGE = 3;
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Pagination/>
+      <div className={styles.posts}>
+        {data?.map((item)=>(
+          <Card key={item._id} item={item}/>
+        ))}
+      </div>
+      <Pagination page={page} hasPrev = {hasPrev}/>
     </div>
   )
 }
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+"use client";
+import React from "react";
 import styles from "./pagination.module.css";
+import { useRouter } from "next/navigation";
 
-const Pagination = () => {
+const Pagination = ({ page, hasPrev }) => {
+  const router = useRouter();
   return (
     <div className={styles.container}>
-      <button className={styles.button}>Previous</button>
-      <button className={styles.button}>Next</button>
+      <button
+        className={styles.button}
+        onClick={() => router.push(`?page=${page - 1}`)}
+        disabled = {!hasPrev}
+      >
+        Previous
+      </button>
+      <button
+        className={styles.button}
+        onClick={() => router.push(`?page=${page + 1}`)}
+      >
+        Next
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Pagination;

5a6aead0e40ea53cee21ca8917cf4a9644a548ce
Astha2410 <asthasrivastava2410@gmail.com>
2023-11-28 20:15:14 +0530

    social media icons uploaded

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.social}>
-        <Image src="/facebook.png" alt='facebook' width={24} height={24}/>
-        <Image src="/instagram.png" alt='facebook' width={24} height={24}/>
+        <Image src="/linkedin.png" alt='linkedin' width={24} height={24}/>
+        <Image src="/instagram.png" alt='instagram' width={24} height={24}/>
         {/* <Image src="/tiktok.png" alt='facebook' width={24} height={24}/> */}
-        <Image src="/youtube.png" alt='facebook' width={24} height={24}/>
+        <Image src="/twitter.webp" alt='twitter' width={24} height={24}/>
       </div>
       <div className={styles.logo}>
         {/* Add your logo image here */}
